9727fc1373d91edf4e02558ce08c37f27311000d
Star24680 <gulerumaan1@gmail.com>
2024-10-15 17:44:07 +0500

    first commit

---
+++ Break.py
@@ -0,0 +1,9 @@
+X=str(input("Enter Word: "))
+for i in X:
+    print (i)
+    if (i =='G'):
+        print (i)
+        print("G is found")
+        break
+    else:
+        print("G is not found")
